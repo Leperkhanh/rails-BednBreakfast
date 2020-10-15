6e5c62838feea579e37236c8692a70abb5abad4c
Leperkhanh <jordannguyen880@gmail.com>
2020-10-15 11:48:27 -0400

    added host to user#new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ protect_from_forgery with: :exception
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :bio])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :bio])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :bio, :host])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :bio, :host])
     end 
 end
