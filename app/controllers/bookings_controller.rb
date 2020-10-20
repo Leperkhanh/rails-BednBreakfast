820131b7eeca2a73c6cb67862c0defae435b3a89
Leperkhanh <jordannguyen880@gmail.com>
2020-10-19 22:35:04 -0400

    update styling on listing#show view and redirect for booking#create controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,7 +3,7 @@ class BookingsController < ApplicationController
         @booking = Booking.new(booking_params)
         if @booking.save
           flash[:notice] = "Booking successfully created"
-          redirect_to root_path
+          redirect_to guest_path(current_guest)
         else
           flash[:alert] = "Booking was not created"
           render 'listings/show'
