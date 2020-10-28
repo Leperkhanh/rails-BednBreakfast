5c8e829781a48d5e8e3208522c99376328d7ec9c
Leperkhanh <jordannguyen880@gmail.com>
2020-10-28 18:23:20 -0400

    add nested listing in routes and update listing#index to include nested listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,12 +1,11 @@
 class ListingsController < ApplicationController
-
   def new
     if user_signed_in?
       @listing = Listing.new
       @listing.build_category
     else
-      flash[:notice] = "You must be logged in to create a new listing!" 
-       redirect_to root_path
+      flash[:notice] = "You must be logged in to create a new listing!"
+      redirect_to root_path
     end
   end
 
@@ -17,13 +16,17 @@ class ListingsController < ApplicationController
       redirect_to @listing
     else
       flash[:alert] = "Listing was not created"
-      render 'new'
+      render "new"
     end
   end
-  
 
   def index
-    @listings = Listing.all
+    @user = User.find_by(id: params[:user_id])
+    if @user
+      @listings = @user.listings
+    else
+      @listings = Listing.all
+    end
     if params[:q] && !params[:q].empty?
       @listings = @listings.search(params[:q].downcase)
     end
@@ -32,63 +35,60 @@ class ListingsController < ApplicationController
   def show
     @listing = Listing.find_by(id: params[:id])
     if @listing
-       @booking = Booking.new
+      @booking = Booking.new
     else
-      redirect_to root_path    
+      redirect_to root_path
     end
   end
 
   def edit
     @listing = Listing.find_by(id: params[:id])
-      if @listing.user == current_user 
-        render 'edit'
-      else
-        flash[:alert] = "You do not have permission to edit this listing!"
-        redirect_to root_path        
-      end
+    if @listing.user == current_user
+      render "edit"
+    else
+      flash[:alert] = "You do not have permission to edit this listing!"
+      redirect_to root_path
+    end
   end
-  
 
   def update
-      @listing = Listing.find(params[:id])
-      if @listing.user == current_user
-        if @listing.update_attributes(listing_params)
-          flash[:notice] = "Listing was successfully updated"
-          redirect_to @listing
-        else
-          flash[:alert] = "Listing was not updated!"
-          render 'edit'
-        end
-      else 
-        flash[:alert] = "You do not have permission to edit this listing"
-        redirect_to root_path     
+    @listing = Listing.find(params[:id])
+    if @listing.user == current_user
+      if @listing.update_attributes(listing_params)
+        flash[:notice] = "Listing was successfully updated"
+        redirect_to @listing
+      else
+        flash[:alert] = "Listing was not updated!"
+        render "edit"
       end
+    else
+      flash[:alert] = "You do not have permission to edit this listing"
+      redirect_to root_path
+    end
   end
-  
+
   def destroy
-      @listing = Listing.find_by(id: params[:id])
-      if @listing.user == current_user
+    @listing = Listing.find_by(id: params[:id])
+    if @listing.user == current_user
       @listing.destroy
-        if @listing.destroy
-          flash[:notice] = 'Listing was successfully deleted.'
-          redirect_to listings_path
-        else
-          flash[:alert] = 'Listing was not deleted'
-          redirect_to listing_path(@listing)
-        end  
+      if @listing.destroy
+        flash[:notice] = "Listing was successfully deleted."
+        redirect_to listings_path
+      else
+        flash[:alert] = "Listing was not deleted"
+        redirect_to listing_path(@listing)
+      end
     else
       flash[:alert] = "You do not have permission to delete this post"
-      redirect_to root_path         
+      redirect_to root_path
     end
-
   end
-  
 
   private
 
   def listing_params
-    params.require(:listing).permit(:title, :description, :price, :bedroom, :bathroom, :address, :city, :state, :img, :category_id, category_attributes: [:name])  
-  end  
+    params.require(:listing).permit(:title, :description, :price, :bedroom, :bathroom, :address, :city, :state, :img, :category_id, category_attributes: [:name])
+  end
 
   def booking_params
     params.require(:booking).permit(:listing_id, :user_id, :guest_id, :comments)
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   resources :bookings
-  root to: 'static#home'
-  get 'static/home'
-  get 'static/signin'
-  get 'static/signup'
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
-    :sessions => "sessions"
-  }
+  root to: "static#home"
+  get "static/home"
+  get "static/signin"
+  get "static/signup"
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks",
+                                    :sessions => "sessions" }
   devise_for :guests
-  resources :users, only: [:index, :show] 
-  resources :guests, only: [:index, :show] 
+  resources :users, only: [:index, :show] do
+    resources :listings
+  end
+  resources :guests, only: [:index, :show]
   resources :listings
   resources :category
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
